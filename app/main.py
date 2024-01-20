21fbcad0283615937316bb67e3b26add680dfb30
ushknn <uekiworks@gmail.com>
2024-01-21 04:00:35 +0900

    add root path for test

--- app/main.py
+++ app/main.py
@@ -9,6 +9,11 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
 # Dependency
 def get_db():
     db = SessionLocal()
